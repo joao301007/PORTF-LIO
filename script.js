65642ece0fdac0adb7d64bed9fb08f1fcaae9917
joao301007 <joaopuskas07@gmail.com>
2025-11-11 18:02:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const body = document.body;
 
     
-    const sunIconPath = '/assets/sun.png';
-    const moonIconPath = '/assets/moon.png';
+    const sunIconPath = './assets/sun.png';
+    const moonIconPath = './assets/moon.png';
 
     function applyTheme(theme) {
         if (theme === 'dark') {
             body.classList.remove('light-mode');
             body.classList.add('dark-mode');
             
-            themeIcon.src = "/assets/sun.png";
+            themeIcon.src = "./assets/sun.png";
             themeIcon.alt = 'Ícone do Sol para ativar o modo claro';
         } else {
             body.classList.remove('dark-mode');
             body.classList.add('light-mode');
          
-            themeIcon.src = "/assets/moon.png";
+            themeIcon.src = "./assets/moon.png";
             themeIcon.alt = 'Ícone da Lua para ativar o modo escuro';
         }
     }
@@ -38,4 +38,5 @@ document.addEventListener('DOMContentLoaded', () => {
         applyTheme(newTheme);
         localStorage.setItem('theme', newTheme);
     });
+
 });
